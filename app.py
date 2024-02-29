fc435197f8ac939eb136929d86b3ed22c33d8d78
deztyy <155615919+deztyy@users.noreply.github.com>
2024-02-29 08:03:40 +0000

    First ver.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+books=[
+    {"id":1,"title":"Book 1","author":"Author 1"},
+    {"id":2,"title":"Book 2","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return "<h1>Hello world</h1>"
+
+
+@app.route("/books",methods=["GET"])
+@cross_origin()
+def get_all_books():
+    return jsonify({"books":books})
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=4000,debug=True)
